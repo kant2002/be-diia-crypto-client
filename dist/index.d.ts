d6c9086e3c5fd441b74f06ea581cddacba8b16eb
Andrii Kurdiumov <kant2002@gmail.com>
2024-03-28 15:53:55 +0500

    Reverse engeneer crypto service API

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './crypto-bankid';
-export * from './crypto-common';
-export * from './crypto-doc';
-export * from './crypto-service';
-export * from './google/protobuf/timestamp';
-//# sourceMappingURL=index.d.ts.map
